2bf39eebdad49526ac78de8c60620f8fa46fc0f5
NullKernel <60713965+make0x20@users.noreply.github.com>
2025-01-23 16:18:11 +0100

    Add nil writer checks

--- logger/logger.go
+++ logger/logger.go
@@ -8,21 +8,23 @@ import (
 
 // New creates a new logger with the given log level and io.writer
 func New(level slog.Level, writers ...io.Writer) *slog.Logger {
-    // If no writers provided, default to stdout
-    if len(writers) == 0 {
-        return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-            Level: level,
-        }))
-    }
+	// Always include stdout writer
+	validWriters := []io.Writer{os.Stdout}
 
-	// Always use stdout
-    writers = append(writers, os.Stdout)
+	// Filter out nil writers
+	for _, w := range writers {
+		if w != nil {
+			validWriters = append(validWriters, w)
+		}
+	}
 
-    // Combine all writers
-    multiWriter := io.MultiWriter(writers...)
-    
-	// Create logger
-    return slog.New(slog.NewTextHandler(multiWriter, &slog.HandlerOptions{
-        Level: level,
-    }))
+	// Combine writers into multiwriter
+	mWriter := io.MultiWriter(validWriters...)
+	// Set log level
+	opts := &slog.HandlerOptions{
+		Level: level,
+	}
+
+	// Create new logger
+	return slog.New(slog.NewTextHandler(mWriter, opts))
 }
